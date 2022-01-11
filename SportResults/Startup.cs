8058385f91eca09bca496446f45865b1ec1fd0dc
Хвищук А.Ю <drunya1996@yandex.ua>
2022-01-11 14:41:49 +0300

    Строка подключения для базы на Heroku

--- SportResults/Startup.cs
+++ SportResults/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SportResults.Models;
+using System;
 
 namespace SportResults
 {
@@ -22,7 +23,8 @@ namespace SportResults
         {
             services.AddControllers();
             services.AddMvc();
-            services.AddDbContext<SportContext>(options => options.UseNpgsql(Configuration.GetConnectionString("SportDatabase")));
+            //services.AddDbContext<SportContext>(options => options.UseNpgsql(Configuration.GetConnectionString("SportDatabase")));
+            services.AddDbContext<SportContext>(options => options.UseNpgsql(this.GetConnection()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +44,28 @@ namespace SportResults
             {
                 endpoints.MapControllers();
             });
+        }
+        private string GetConnection()
+        {
+            // Heroku provides PostgreSQL connection URL via env variable
+            var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            // Parse connection URL to connection string for Npgsql
+            connUrl = connUrl.Replace("postgres://", string.Empty);
+
+            var pgUserPass = connUrl.Split("@")[0];
+            var pgHostPortDb = connUrl.Split("@")[1];
+            var pgHostPort = pgHostPortDb.Split("/")[0];
+
+            var pgDb = pgHostPortDb.Split("/")[1];
+            var pgUser = pgUserPass.Split(":")[0];
+            var pgPass = pgUserPass.Split(":")[1];
+            var pgHost = pgHostPort.Split(":")[0];
+            var pgPort = pgHostPort.Split(":")[1];
+
+            string connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+
+            return connStr;
         }
     }
 }
